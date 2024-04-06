1e210c9cdc9058087fb4183dd30c2851e924f659
Laura <garch.laura@gmail.com>
2024-04-07 01:04:11 +0200

    game

--- game.py
+++ game.py
@@ -1,16 +1,58 @@
 import random
 
-def oneNumCorrect(num):
-    digits = [int(d) for d in str(num)]
+def twoNumsCorrectWrongPlace(num):
+	digits = [int(d) for d in str(num)]
+	chose1 = digits[0]
+	chose2 = digits[2]
+	errnums = [n for n in range(10) if n not in digits]
+	num1 = random.choice(errnums)
 
-    firstDigit = digits[0];
-    newNum = num;
+	newDigits = chose2, chose1, num1
+	res = "".join(map(str, newDigits)).zfill(3)
+	return res
 
-    while True:
-        newNum = random.randint(100, 999)
-        newDigits = [int(d) for d in str(newNum)]
-        if len(set(newDigits)) == 3:
-            return (newNum)
+def oneNumCorrectCorrectPlace(num):
+	digits = [int(d) for d in str(num)]
+	randomidx = random.randint(0, 2)
+	chose = digits[randomidx]
+
+	digits.pop(randomidx)
+	errnums = [n for n in range(10) if n != chose and n not in digits]
+	num1 = random.choice(errnums)
+	errnums.remove(num1)
+	num2 = random.choice(errnums)
+
+	digits.remove(digits[0])
+	digits.remove(digits[0])
+
+	digits.insert(0, num1)
+	digits.insert(1, num2)
+	digits.insert(randomidx, chose)
+	res = "".join(map(str, digits)).zfill(3)
+	return (res)
+
+def oneNumCorrectWrongPlace(num):
+	digits = [int(d) for d in str(num)]
+	randomidx = random.randint(0, 2)
+	chose = digits[randomidx]
+
+	digits.pop(randomidx)
+	
+	errnums = [n for n in range(10) if n != chose and n not in digits]
+	num1 = random.choice(errnums)
+	errnums.remove(num1)
+	num2 = random.choice(errnums)
+
+	digits.remove(digits[0])
+	digits.remove(digits[0])
+	
+	digits.insert(0, num1)
+	digits.insert(1, num2)
+	
+	newpos = 0 if randomidx == 2 else 2
+	digits.insert(newpos, chose)
+	res = "".join(map(str, digits)).zfill(3)
+	return res
 
 def resultNum():
     while True:
@@ -29,8 +71,19 @@ def checkIncorrectNums(num):
 
 num = resultNum();
 noNum = checkIncorrectNums(num);
-oneNum = oneNumCorrect(num);
+oneNumWrongPlace = oneNumCorrectWrongPlace(num);
+oneNumWrongPlace2 = oneNumCorrectWrongPlace(num);
+oneNumCorrectPlace = oneNumCorrectCorrectPlace(num);
+twoNumIncorrectPlace = twoNumsCorrectWrongPlace(num)
 
-print (num);
-print (str(noNum) + " => todos los numeros son incorrectos")
-print (str(oneNum) + " => un numero es correcto pero en el sitio incorrecto")
+print (f"{noNum} => todos los numeros son incorrectos")
+print (f"{oneNumWrongPlace} => un numero es correcto pero en el sitio incorrecto")
+print (f"{oneNumWrongPlace2} => un numero es correcto pero en el sitio incorrecto")
+print (f"{oneNumCorrectPlace} => un numero es correcto y en el sitio correcto")
+print (f"{twoNumIncorrectPlace} => dos numeros son correctos en el sitio incorrecto")
+
+inp = input("Cual es el numero? ")
+if int(inp) == int(num):
+	print("Correcto!")
+else:
+	print(f"Incorrecto, el numero era {num}")
